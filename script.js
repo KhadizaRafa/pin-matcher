19dc8bb7f30a364201f2b4ec13de5a16e17da9e6
Khadiza Rafa <khadiza301rafa@gmail.com>
2020-08-04 03:11:04 +0600

    Added pin generator, match feature, number of retries per session

---
+++ script.js
@@ -0,0 +1,93 @@
+let inputDigit = 0;
+let retryPinCount = 3;
+
+function pinGenerator()
+{
+    const startValue = 1000;
+    const endValue = 10000;
+    const range = endValue - startValue;     
+    var pin = Math.floor(range * Math.random()) + startValue; //fourDigitRandomValue = startValue + Math.floor((endValue - startValue) * random)
+    document.getElementById('view-pin').value = pin;
+}
+    
+function displayDigit(digit)
+{
+    if((inputDigit.toString()).length!=4)
+    {
+        inputDigit = inputDigit * 10 + digit;     
+    }
+    document.getElementById('provided-pin').value = inputDigit;
+}
+
+function matchPin()
+{
+    var generatedPin = document.getElementById('view-pin').value;
+    var providedPin = document.getElementById('provided-pin').value;
+
+    if(generatedPin-providedPin==0){
+        displayStyleBlock('rightPin',true);
+        displayStyleBlock('wrongPin',false);
+        displayStyleBlock('retryPin',false);
+
+    }
+    else{
+        retryPinCount = retryPinCount - 1;
+        displayStyleBlock('wrongPin',true);
+        displayStyleBlock('retryPin',true);
+        displayStyleBlock('rightPin',false);
+        
+        if(retryPinCount>0)
+        {
+            document.getElementById('retryPin').innerText = retryPinCount +' try left';
+        }        
+        else
+        {
+            document.getElementById('submitPin').disabled = true;
+            document.getElementById('generate-pin').disabled = true;
+            document.getElementById('retryPin').innerText = 'No more retry allowed for this session';
+        }
+    }
+    clearPin('view-pin');
+    clearPin('provided-pin');
+
+}
+
+function displayStyleBlock(id,isBlock)
+{
+    if(isBlock)
+      document.getElementById(id).style.display = "block";
+    else
+      document.getElementById(id).style.display = "none";
+}
+function clearPin(id)
+{
+    if(id=='provided-pin')
+    {
+        inputDigit='';
+    }
+    document.getElementById(id).value = "";
+}
+
+
+function backSpace()
+{
+    inputDigit = parseInt(inputDigit/10);
+    if(inputDigit==0)
+    {
+        inputDigit = '';
+    }
+    document.getElementById('provided-pin').value = inputDigit;
+}
+
+
+
+// function backSpace()
+// {
+//     var providedPinVal = document.getElementById('provided-pin').value;
+//     var providedPinValStr = providedPinVal.toString();
+//     var array = Array.from(providedPinValStr);
+//     array.pop();
+//     var newVal = array.join('');
+//     console.log(newVal);
+//     document.getElementById('provided-pin').value = newVal;
+// }
